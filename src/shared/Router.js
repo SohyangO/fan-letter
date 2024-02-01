5b19221d6ab2228f8dcd99f6606fc764531fee28
SohyangO <shyang903@naver.com>
2024-02-01 22:04:15 +0900

    상세페이지 클릭 시 이동 코드 리팩토링

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LetterList from "components/LetterList";
 import Home from "pages/home";
 import Detail from "pages/detail";
 import { useState } from "react";
